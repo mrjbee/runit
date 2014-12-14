015f1c1f013916c174aa8324e03d7a1fb014fe2f
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2014-12-14 15:11:45 +0200

    Fix overhead issue

--- sources/app/src/main/java/org/monroe/team/android/box/manager/BackgroundTaskManager.java
+++ sources/app/src/main/java/org/monroe/team/android/box/manager/BackgroundTaskManager.java
@@ -1,21 +1,15 @@
 package org.monroe.team.android.box.manager;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Observer;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.RejectedExecutionException;
 
 public class BackgroundTaskManager {
 
     private final ExecutorService taskExecutionService = Executors.newCachedThreadPool();
-    private final ExecutorService notificationExecutionService = Executors.newCachedThreadPool();
-    private Future notificationTaskFuture;
-    private List<BackgroundTask> awaitingTaskList = new ArrayList<BackgroundTask>();
 
     final public synchronized <ResultType> BackgroundTask<ResultType> execute(
             Callable<ResultType> callable) {
@@ -25,62 +19,17 @@ public class BackgroundTaskManager {
     final public synchronized <ResultType> BackgroundTask<ResultType> execute(
                                 Callable<ResultType> callable,
                                 TaskCompletionNotificationObserver<ResultType> observer) {
-
-        Future<ResultType> future = taskExecutionService.submit(callable);
-        BackgroundTask<ResultType> answer = new BackgroundTask<ResultType>(future,observer);
-        if (observer != null)
-            scheduleResultCheck(answer);
+        Future<ResultType> future = taskExecutionService.submit(new CoverCallable<ResultType>(callable, observer));
+        BackgroundTask<ResultType> answer = new BackgroundTask<ResultType>(future, observer);
         return answer;
-
-    }
-
-    private <ResultType> void scheduleResultCheck(BackgroundTask<ResultType> answer) {
-        markBackgroundTask(answer);
-        if (notificationTaskFuture == null || notificationTaskFuture.isDone()){
-            notificationTaskFuture = taskExecutionService.submit(new Callable() {
-                @Override
-                public Object call() throws Exception {
-                    while (!Thread.currentThread().isInterrupted() && awaitingTaskList != null){
-                        List<BackgroundTask> awaitingTaskSnapshot = new ArrayList<BackgroundTask>(awaitingTaskList);
-                        List<BackgroundTask> tasksToRemoveList = new ArrayList<BackgroundTask>();
-                        for (BackgroundTask backgroundTask : awaitingTaskSnapshot) {
-                            try {
-                                Object result = backgroundTask.actualFuture.get(100, TimeUnit.MILLISECONDS);
-                                tasksToRemoveList.add(backgroundTask);
-                                backgroundTask.observer.onSuccess(result);
-                            }
-                            catch (TimeoutException e){/*do nothing just go to next*/}
-                            catch (Exception e){
-                                backgroundTask.observer.onFails(e);
-                                tasksToRemoveList.add(backgroundTask);
-                            }
-                        }
-                        for (BackgroundTask backgroundTask : tasksToRemoveList) {
-                            unmarkBackgroundTask(backgroundTask);
-                        }
-                    }
-                    return null;
-                }
-            });
-        }
-    }
-
-    private synchronized void markBackgroundTask(BackgroundTask backgroundTask) {
-        awaitingTaskList.add(backgroundTask);
-    }
-
-    private synchronized void unmarkBackgroundTask(BackgroundTask backgroundTask) {
-        awaitingTaskList.remove(backgroundTask);
     }
 
     public static class BackgroundTask <Result> {
 
         private final Future<Result> actualFuture;
-        private final TaskCompletionNotificationObserver<Result> observer;
 
         public BackgroundTask(Future<Result> actualFuture, TaskCompletionNotificationObserver<Result> observer) {
             this.actualFuture = actualFuture;
-            this.observer = observer;
         }
 
         public void cancel() {
@@ -93,4 +42,31 @@ public class BackgroundTaskManager {
         public void onFails(Exception e);
     }
 
+    private static class CoverCallable<ResultType> implements Callable<ResultType>{
+
+        private final Callable<ResultType> originalCallable;
+        private final TaskCompletionNotificationObserver<ResultType> observer;
+
+        private CoverCallable(Callable<ResultType> originalCallable, TaskCompletionNotificationObserver<ResultType> observer) {
+            this.originalCallable = originalCallable;
+            this.observer = observer;
+        }
+
+        @Override
+        public ResultType call() throws Exception {
+            ResultType result =  null;
+            try {
+                result = originalCallable.call();
+            } catch (Exception e){
+                if (observer != null){
+                    observer.onFails(e);
+                }
+                throw e;
+            }
+            if (observer != null) {
+                observer.onSuccess(result);
+            }
+            return result;
+        }
+    }
 }
